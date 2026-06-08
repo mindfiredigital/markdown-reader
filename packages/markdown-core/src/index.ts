44aff76667fd363f1f2a8ea6eaed049308f269eb
Ashminita <ashminitab@mindfiresolutions.com>
2026-06-08 15:19:47 +0530

    feat(markdown-core): add entry file for markdown core functions

---
+++ packages/markdown-core/src/index.ts
@@ -0,0 +1,7 @@
+export { resetHilighter, shikiHighlighter } from './renderer/shiki';
+export { extractTOC } from './renderer/toc';
+export { renderMermaid } from './renderer/mermaid';
+export { renderMarkdown } from './renderer/markdown';
+export { renderInlineMath, renderBlockMath } from './renderer/katex';
+export { parseCallouts } from './renderer/callout';
+export { saveScrollPos, getScrollPos, clearScrollPos } from './renderer/scroll';
