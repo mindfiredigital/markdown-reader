3435dee7d83f58cbc21b6a4e8957c86d63bd3a03
Ashminita <ashminitab@mindfiresolutions.com>
2026-06-10 17:27:28 +0530

    refactor(extension): update vite build config

---
+++ apps/extension/vite.config.ts
@@ -0,0 +1,22 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        viewer: resolve(__dirname, 'viewer.html'),
+        popup: resolve(__dirname, 'popup.html'),
+      },
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name][extname]',
+      },
+    },
+  },
+});
