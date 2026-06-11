8f86166cce994e2245d618c5106b1787acb80510
Ashminita <ashminitab@mindfiresolutions.com>
2026-06-11 16:15:43 +0530

    fix(renderer): remove katex inport from renderer entry point

--- apps/renderer/src/main.tsx
+++ apps/renderer/src/main.tsx
@@ -5,7 +5,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import "./index.css";
 import { ThemeProvider } from './context/ThemeProvider'
-import 'katex/dist/katex.min.css';
 import { TabProvider } from './context/TabProvider';
 import { ErrorBoundary } from './components/ErrorBoundary';
 
