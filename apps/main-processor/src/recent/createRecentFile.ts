7b981175159951857e8450b75b7e95f6e4e7b62b
Ashminita <ashminitab@mindfiresolutions.com>
2026-06-22 07:59:07 +0530

    feat(main-processor): add functionality to add file sizes to the recent files

--- apps/main-processor/src/recent/createRecentFile.ts
+++ apps/main-processor/src/recent/createRecentFile.ts
@@ -1,10 +1,19 @@
 import { basename } from 'node:path';
 import { RecentFile } from '@package/shared-types';
+import { statSync } from 'node:fs';
 
 export function createRecentFile(filePath: string): RecentFile {
+  let size: number | undefined;
+  try {
+    const stats = statSync(filePath);
+    size = stats.size;
+  } catch {
+    // ignore error if file doesnot exists
+  }
   return {
     path: filePath,
     name: basename(filePath),
     openedAt: Date.now(),
+    ...(size !== undefined ? { size } : {}),
   };
 }
