3bec45e90736e2fed059b9c4926087c1578207e2
Ashminita <ashminitab@mindfiresolutions.com>
2026-06-26 16:51:47 +0530

    feat(renderer): add component to display raw markdown

---
+++ apps/renderer/src/components/RawTextViewer.tsx
@@ -0,0 +1,10 @@
+import { RawTextViewerProps } from "../types/component-types";
+
+// It renders raw Markdown text in formatted code-style layout
+export function RawTextViewer({ markdown }: RawTextViewerProps) {
+  return (
+    <pre className="p-8 whitespace-pre-wrap wrap-break-word font-mono text-sm leading-relaxed max-w-4xl mx-auto">
+      {markdown ?? ''}
+    </pre>
+  );
+}
